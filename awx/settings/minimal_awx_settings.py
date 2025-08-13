5246dd61d6e169189c5b2b9506e4d81e438e5c3e
Radhouene1 <bha.radhouene@gmail.com>
2025-08-13 11:21:24 -0400

    add PYTHON PATH+2

--- awx/settings/minimal_awx_settings.py
+++ awx/settings/minimal_awx_settings.py
@@ -1,11 +1,17 @@
-from awx.settings.default import *
+# awx/settings/minimal_awx_settings.py
+from awx.settings.defaults import *
 
-SECRET_KEY = 'dummy'
-DEBUG = True
-
+# Required for minimal build
+SKIP_PRODUCTION_CHECKS = True
+INSTALLED_APPS = ['awx.main']  # minimum needed
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/tmp/awx.db',
+        'NAME': ':memory:',
     }
 }
+AWX_IS_K8S = False
+DEBUG = True
+
+# Add anything else referenced by default_settings
+DEFAULTS_SNAPSHOT = {}  # this is mandatory
